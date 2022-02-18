259e4c5bf4cbc60a92e7842501efe52a7620f9d3
Favourosas <favourogbeiwi@gmail.com>
2022-02-18 21:38:38 +0200

    my first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+for (i=0; i < 10; i ++)
+console.log(`the is the ${i} mistake`)
+hgryhfhhhdfsfjfjhdjksfbkjfbkjgjdwhbdsvhbdahvjdbkjdsvnbsfkjhkgefkjsfnbfdn this.setState((state, props) => { return { nhhded }});
+console.log(favour)hgryhfhhhdfsfjfjhdjksfbkjfbkjgjdwhbdsvhbdahvjdbkjdsvnbsfkjhkgefkjsfnbfdnvvvvvv
+kjhfgb
+ghdsvhdfjkrghjsjkrhjttuh
